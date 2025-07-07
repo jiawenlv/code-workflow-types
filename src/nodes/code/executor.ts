9fcac6fc1aeca6c8f421407defaee458211b1caa
吕嘉文 <daughlas@gmail.com>
2025-07-07 17:40:07 +0800

    refactor(executor): update executeCodeNode function to accept inputs and filePath
    - Modified executeCodeNode to take inputs and filePath as parameters instead of nodeConfig.
    - Updated the import logic to dynamically load the script based on the provided filePath.
    - Enhanced the function to log the file content and execute it with the given inputs.

--- src/nodes/code/executor.ts
+++ src/nodes/code/executor.ts
@@ -1,14 +1,12 @@
 import { CodeNode } from "./ui.schema";
 
-export const executeCodeNode = async (nodeConfig: CodeNode) => {
-    const { inputs } = nodeConfig.data;
-    const script = inputs.find(input => input.attrName === "script")?.value;
-    const path = inputs.find(input => input.attrName === "path")?.value;
+export const executeCodeNode = async (inputs: any, filePath: string) => {
 
-    // TODO get absolute Path
-    const theScript = await import(path)
-    // TODO 解析安装依赖
+    const fileContent = await import(filePath)
 
-    const result = theScript.default(script);
+    console.log(fileContent)
+
+ 
+    const result = fileContent.default(inputs);
     return result;
 }
